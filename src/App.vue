a58a68de695b5788a3ca851d6fdcd71b7624f2cc
Jason Ermer <jasonermer@gmail.com>
2023-04-05 14:44:04 -0700

    add about modal

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 // import { RouterLink, RouterView } from 'vue-router'
 import CharBuilder from './components/CharBuilder.vue'
 import PDFButton from './components/PDFButton.vue'
+import AboutModal from './components/AboutModal.vue'
 </script>
 
 <script>
@@ -27,37 +28,24 @@ export default {
   <header class="pt-5">
     <div class="title">
       <h1>Heroes Without the Handbook</h1>
-      <div class="lead">Streamlined character creation based on the world's most well-known roleplaying game.</div>
+      <div class="lead">Streamlined hero creation based on the world's most well-known roleplaying game.</div>
     </div>
 
     <hr />
 
     <div class="instructions">
       <h5>- INSTRUCTIONS -</h5>
-      <p>Make some decisions about your character using the sections shown to the right. When you're done, click the
+      <p>Make decisions about your hero using the sections to the right. When you're done, click the
         button
-        to download a character sheet!</p>
+        to download your hero booklet!</p>
 
       <PDFButton />
+
+      <p class="small mt-4"><a href="#" data-bs-toggle="modal" data-bs-target="#aboutModal">ABOUT</a></p>
+      <AboutModal />
     </div>
 
-    <img alt="Mythematics logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="disclaimer mt-5">
-      <p>
-        <i>Heroes Without the Handbook</i> is by Jason Ermer and available at <a href="https://mythematics.org/heroes/"
-          target="_blank" rel="noopener">mythematics.org/heroes</a>.
-      </p>
-      <p>
-        This work includes material taken from the System Reference Document 5.1 ("SRD 5.1") by Wizards of the Coast LLC
-        and available at
-        <a href="https://dnd.wizards.com/resources/systems-reference-document" target="_blank"
-          rel="noopener">dnd.wizards.com/resources/systems-reference-document</a>. The SRD 5.1 is licensed under the
-        <a href="https://creativecommons.org/licenses/by/4.0/legalcode" target="_blank" rel="noopener">Creative Commons
-          Attribution 4.0 International License</a>.
-      </p>
-
-    </div>
+    <!-- <img alt="Mythematics logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
   </header>
 
   <main>
@@ -79,7 +67,7 @@ main {
 }
 
 .title {
-  color: darkblue
+  color: #0d6efd;
 }
 
 .instructions {
@@ -89,11 +77,5 @@ main {
 .logo {
   display: block;
   margin: 5rem auto 0;
-}
-
-.disclaimer {
-  color: gray;
-  text-align: left;
-  font-size: x-small;
 }
 </style>
---
+++ src/components/AboutModal.vue
@@ -0,0 +1,44 @@
+
+<template>
+    <div id="aboutModal" class="modal" tabindex="-1">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body about">
+                    <p>
+                        <i>Heroes Without the Handbook</i> is by Jason Ermer. More information about the project is
+                        available at <a href="https://mythematics.org/heroes/" target="_blank"
+                            rel="noopener">mythematics.org/heroes</a>.
+                    </p>
+                    <p>
+                        This work includes material taken from the System Reference Document 5.1 ("SRD 5.1") by Wizards of
+                        the Coast LLC
+                        and available at
+                        <a href="https://dnd.wizards.com/resources/systems-reference-document" target="_blank"
+                            rel="noopener">dnd.wizards.com/resources/systems-reference-document</a>. The SRD 5.1 is licensed
+                        under the
+                        <a href="https://creativecommons.org/licenses/by/4.0/legalcode" target="_blank"
+                            rel="noopener">Creative Commons
+                            Attribution 4.0 International License</a>.
+                    </p>
+                    <p>
+                        The favicon is <a href="https://game-icons.net/1x1/lorc/winged-sword.html" target="_blank"
+                            rel="noopener">Winged Sword</a> by Lorc, available from <a href="https://game-icons.net/"
+                            target="_blank" rel="noopener">game-icons.net</a> and used
+                        under a <a href="https://creativecommons.org/licenses/by/3.0/" target="_blank" rel="noopener">CC-BY
+                            3.0</a> license.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+.about {
+    text-align: left;
+    font-size: small;
+}
+</style>
