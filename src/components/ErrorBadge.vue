557ca59887072d54f530ff4698d1c119252bcc47
Jason Ermer <jasonermer@gmail.com>
2023-07-07 09:31:22 -0700

    adjust error badge placement

--- src/components/ErrorBadge.vue
+++ src/components/ErrorBadge.vue
@@ -1,6 +1,6 @@
 
 <template>
-    <span class="position-absolute top-50 start-0 translate-middle badge bg-warning">
+    <span class="badge bg-warning mx-2">
         <font-awesome-icon icon="fa-solid fa-triangle-exclamation" style="color: black;" />
         <span class="visually-hidden">section alert</span>
     </span>
